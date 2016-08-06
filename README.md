edda4b1837c9e6bfc5772afff15073d36c53d857
Shivam Satija <dr.droid27@gmail.com>
2016-08-06 23:47:57 +0530

    READEM.md Modified

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-```Perfecto - Networking library
+#Perfecto - Networking library
+
+##Use Case for "GET" Request
+```java
+				
+		Perfecto.with(context)
+            .fromUrl("https://www.google.co.in")
+            .ofTypeGet()
+            .connect(new OnRequestComplete() {
+               
+                @Override
+                public void onSuccess(String response) {
+                    Log.d("Response", response);    
+                }
+
+                @Override
+                public void onFailure(String error) {
+                    /**
+                    *  This will return the errorStream(), responseCode and responseMessage
+                    *  Use Log to get the error.
+                    */
+                      
+                }
+            });
+```
+
 
--- app/src/main/java/com/algowire/perfecto/MainActivity.java
+++ app/src/main/java/com/algowire/perfecto/MainActivity.java
@@ -1,15 +1,41 @@
 package com.algowire.perfecto;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import developer.shivam.perfecto.OnRequestComplete;
+import developer.shivam.perfecto.Perfecto;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Perfecto.with(context)
+                .fromUrl("https://www.google.co.in")
+                .ofTypeGet()
+                .connect(new OnRequestComplete() {
+
+                    @Override
+                    public void onSuccess(String response) {
+                        Log.d("Response", response);
+                    }
+
+                    @Override
+                    public void onFailure(String error) {
+                        /**
+                         *  This will return the errorStream(), responseCode and responseMessage
+                         *  Use Log to get the error.
+                         */
+
+                    }
+                });
 
     }
 }
